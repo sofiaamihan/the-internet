ea49e6ff823a30ecdfb5c017b88cba5ee01ee3db
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-11-07 08:16:08 +0800

    Global Configurations

--- conftest.py
+++ conftest.py
@@ -1,7 +1,23 @@
 import pytest
 from selenium import webdriver
+import allure
 
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # This hook is called after each test phase (setup, call, teardown)
+    outcome = yield
+    result = outcome.get_result()
 
+    if result.when == "call" and result.failed:
+        driver = item.funcargs.get("driver")
+        if driver:
+            allure.attach(
+                driver.get_screenshot_as_png(),
+                name="screenshot_on_failure",
+                attachment_type=allure.attachment_type.PNG
+            )
+
+@allure.title("Setting up Driver")
 @pytest.fixture()
 def driver():
     chrome_driver = webdriver.Chrome()
---
+++ test_basic_auth.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+
+class TestBasicAuth:
+
+    @pytest.mark.login
+    @pytest.mark.parametrize("username, password",[
+        ("admin", "admin"),
+        ("incorrect-username", "admin"),
+        ("admin", "incorrect-password"),
+        ("incorrect", "incorrect")
+    ], ids=['correct-login', 'incorrect-username', 'incorrect-password', 'incorrect-login'])
+    def test_all_login_cases(self, driver, username, password):
+        URL = "https://" + username + ":" + password + "@" + "the-internet.herokuapp.com/basic_auth"
+        driver.get(URL)
+        # wait = WebDriverWait(driver, 10)
+        text_locator = driver.find_element(By.XPATH, "//div[@class='example']/p")
+        assert text_locator.is_displayed()
