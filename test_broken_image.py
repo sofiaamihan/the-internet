e187c44ab164d5d879ebadd165dcb9777976c8f0
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-11-08 12:59:21 +0800

    Broken Images Tests

---
+++ test_broken_image.py
@@ -0,0 +1,18 @@
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+
+class TestBrokenImages:
+
+    @pytest.mark.image
+    def test_broken_images(self, driver):
+        driver.get("https://the-internet.herokuapp.com/broken_images")
+        images = driver.find_elements(By.XPATH, "//div[@id='content']/div[@class='example']/img")
+
+        image_1_has_loaded = driver.execute_script("return arguments[0].complete && arguments[0].naturalWidth > 0;", images[0])
+        image_2_has_loaded = driver.execute_script("return arguments[0].complete && arguments[0].naturalWidth > 0;", images[1])
+        image_3_has_loaded = driver.execute_script("return arguments[0].complete && arguments[0].naturalWidth > 0;", images[2])
+
+        assert [True if image_1_has_loaded == False else False]
+        assert [True if image_2_has_loaded == False else False]
+        assert image_3_has_loaded
