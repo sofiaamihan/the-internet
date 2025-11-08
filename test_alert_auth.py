4a6d048ccdf43fb88fecd61baa7e2cc4831a79e5
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-11-08 12:19:23 +0800

    Slight Modifications

--- test_alert_auth.py
+++ test_alert_auth.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
-# TODO - complex authentication with error checks, locking outs, etc
+# TODO - complex authentication with error checks, locking outs, etc, through forms as well
 
 class TestBasicAuth:
 
