6bbe70e743e5d4cd738ce6a91af2dfd1ae7b86fe
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-11-10 08:20:22 +0800

    CSS Styles Test

--- test_disappearing_elements.py
+++ test_disappearing_elements.py
@@ -1,8 +1,8 @@
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 import allure
 import pytest
 
-# TODO - experimenting with styles, how to check the css
 # TODO - elements that appear on a screen for a fixed amount of time, check that too
 
 class TestDisappearingElements:
@@ -24,3 +24,29 @@ class TestDisappearingElements:
             )
 
             assert length >= 4, f"Expected at least 4 elements, found {length}"
+
+    @pytest.mark.elements
+    def test_styles_elements(self, driver):
+        driver.get("https://the-internet.herokuapp.com/disappearing_elements")
+
+        elements = driver.find_elements(By.XPATH, "//div[@class='example']/ul/li/a")
+        home_element = elements[0]
+
+        allure.attach(
+            f"Colour of element: {home_element.value_of_css_property('color')}",
+             name=f"Colour",
+             attachment_type=allure.attachment_type.TEXT
+        )
+
+        assert home_element.value_of_css_property('color') == "rgba(218, 75, 75, 1)"
+
+        actions = ActionChains(driver)
+        actions.move_to_element(home_element).perform()
+
+        allure.attach(
+            f"Colour of element: {home_element.value_of_css_property('color')}",
+            name=f"Colour",
+            attachment_type=allure.attachment_type.TEXT
+        )
+
+        assert home_element.value_of_css_property('color') == "rgba(0, 0, 0, 1)"
