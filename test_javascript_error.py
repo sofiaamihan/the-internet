1eb80f659b04a227ac016c9e91efd8b37cb0f4fd
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-11-11 14:58:06 +0800

    JS Onload Errors Tests

---
+++ test_javascript_error.py
@@ -0,0 +1,24 @@
+import pytest
+import allure
+
+# TODO - what are the different kinds of entry levels???
+
+class TestJavascriptError:
+
+    @pytest.mark.error
+    def test_javascript_error(self, driver):
+
+        driver.get("https://the-internet.herokuapp.com/javascript_error")
+        logs = driver.get_log('browser')
+
+        errors = [entry for entry in logs if entry['level'] in ('SEVERE', 'ERROR')]
+
+        for e in errors:
+            allure.attach(
+                f"Javascript Error: {e}",
+                name="JS Error",
+                attachment_type=allure.attachment_type.TEXT
+            )
+
+        assert len(errors) != 0
+
