cb9e9ccd754760687183d4bee7f8aee83f4b0527
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-12-13 15:34:25 +0800

    Implement Testing Grounds and Improve Shadow DOM

---
+++ test_dump.py
@@ -0,0 +1,63 @@
+import os.path
+import allure
+import pytest
+import time
+import random
+from selenium import webdriver
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+# ----------General Questions ----------
+# What are the other options and would it
+# Expand on the different possible options for webdriver
+
+# JS - Conversion of data types and manipulating arrays
+# API - Timed practice for goal tracker < 45 minutes
+# ISSUE - its not letting me create a new project or what
+# XPath - find items, choose index 4 then expand on the children from there?
+# XPath - how to get <path> element
+
+# always search for the keyword, go with industry standard
+# loop through pages and search for something
+# dropdowns where the options disappear once you move away or interact elsewhere
+# How come sometimes jmeter runs but sometimes some weird error is their like index out of bounds then when i run it again it's like fine
+# is it normal for the test to sometimes work, sometimes not, but maybe wait should be implemented
+
+# ---------- Revision for Arrays ----------
+# Lists, For Loops, and Dictionaries
+# Reading - Reverse Order
+# Reading - Intervals
+# Reading - Specific Range
+# Reading - Something that doesn't exist (if applicable)
+# Add - At the end
+# Add - In front
+# Add - In the middle
+# Add - In missing index (1, 3, 4) so insert in index 2
+# Edit - Something that exists
+# Edit - Something that doesn't exist
+# Edit - In front
+# Edit - In the end
+# Edit - Everything
+# Delete - Everything
+# Delete - Specific thing
+# Delete - In front
+# Delete - In the end
+# Delete - Something that doesn't exist
+# Sorting keys / Re-ordering something
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test_shadow_dom.py
+++ test_shadow_dom.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
 import pytest
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 class TestShadowDOM:
 
@@ -18,3 +20,19 @@ class TestShadowDOM:
 
         line_3 = driver.find_elements(By.CSS_SELECTOR, "ul[slot='my-text']")[0].text.split('\n')[1]
         assert line_3 == "In a list!"
+
+    def test_shadow_dom2(self, driver):
+        driver.get("https://practice.expandtesting.com/shadowdom")
+        wait = WebDriverWait(driver, 10)
+
+        # 1 - Get Parent Id
+        wait.until(ec.visibility_of_element_located((By.XPATH, "//div[@id='shadow-host']")))
+        parent = driver.find_element(By.XPATH, "//div[@id='shadow-host']")
+
+        # 2 - Get Shadow DOM via JS
+        shadow = parent.shadow_root
+
+        # 3 - Access hidden element
+        hidden = shadow.find_element(By.ID, "my-btn")
+
+        assert hidden.is_displayed()
